5262dd0292345c17111534a2a7211d234f8ec5b3
Marko Papic <marko@markopapic.com>
2021-03-04 11:40:04 +0100

    Use IntPtr for x64 compat. Enable C++ debugging.

--- CsharpClient/Program.cs
+++ CsharpClient/Program.cs
@@ -6,12 +6,12 @@ namespace CsharpClient
     class Program
     {
         // You can omit the .dll extension. This is advised for cross-platform builds.
-        [DllImport("Core", CallingConvention = CallingConvention.Cdecl, CharSet = CharSet.Unicode, SetLastError = true)]
-        private static extern int add(int a, int b);
+        [DllImport("Core", CallingConvention = CallingConvention.Cdecl)]
+        private static extern IntPtr add(int a, int b);
 
         static void Main(string[] args)
         {
-            int res = add(5, 4);
+            IntPtr res = add(5, 4);
             Console.WriteLine(res);
         }
     }
